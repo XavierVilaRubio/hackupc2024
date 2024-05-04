5e8a14bf1f8fdf2ad834eddb53e4581c7f893eab
Xavier Vila <xvrvilarubio2001@gmail.com>
2024-05-04 15:28:53 +0200

    working

--- app.vue
+++ app.vue
@@ -9,15 +9,15 @@ enum Topics {
 }
 
 onMounted(() => {
-  client.value = mqtt.connect('mqtt://192.168.187.60:8080')
+  client.value = mqtt.connect('mqtt://localhost:8080')
 
-  client.value.on('connect', () => {
+  client.value.on('connect', () =>
     client.value?.subscribe(Topics.Events, (err) => {
       if (!err) {
-        client.value?.publish('patata', 'Hello mqtt')
+        client.value?.publish(Topics.Events, 'testiiiing')
       }
     })
-  })
+  )
 
   client.value.on('message', (topic, message) => {
     // message is Buffer
@@ -26,74 +26,87 @@ onMounted(() => {
 })
 
 const state = ref('00')
-const led = computed(() => state.value.at(0) === '1')
-const sensor = computed(() => state.value.at(1) === '1')
+// const led = computed(() => state.value.at(0) === '1')
+const led = ref(false)
+// const sensor = computed(() => state.value.at(1) === '1')
+const sensor = ref(false)
 
 // enum Sensors {
 //   LED = 0,
 //   SENSOR = 1,
 // }
 
-const status = ref('')
 const send = (type: 'led' | 'sensor') => {
   switch (type) {
     case 'led':
+      led.value = !led.value
       client.value?.publish(
         Topics.Commands,
         `${led.value ? '0' : '1'}${state.value.at(1)}`
       )
-      // socket.value?.send(`${led.value ? '0' : '1'}${state.value.at(1)}`)
       break
     case 'sensor':
+      sensor.value = !sensor.value
       client.value?.publish(
         Topics.Commands,
         `${state.value.at(0)}${sensor.value ? '0' : '1'}`
       )
-      // socket.value?.send(`${state.value.at(0)}${sensor.value ? '0' : '1'}`)
       break
   }
 }
-// const send2 = (type: Sensors) => {}
 </script>
 <template>
-  <div class="w-screen h-screen grid place-content-center gap-8">
-    <p>{{ status }}</p>
-    <UiButton @click="() => send('led')">test</UiButton>
-    <div class="min-w-96 space-y-4">
-      <div class="flex gap-2">
-        <UiToggle
-          class="flex-1"
-          variant="outline"
-          :pressed="led"
-          @update:pressed="() => send('led')"
-        >
-          {{ led ? 'On' : 'Off' }}
-        </UiToggle>
-        <div class="size-10 *:m-auto content-center">
-          <IconsLightBulbFilled v-if="led" />
-          <IconsLightBulb v-else />
-        </div>
-      </div>
-      <UiButton
-        @mousedown="() => send('sensor')"
-        @mouseup="() => send('sensor')"
-      >
-        Press
-      </UiButton>
-      <div class="flex gap-2">
-        <UiToggle
-          class="flex-1"
-          variant="outline"
-          :pressed="sensor"
-          @update:pressed="() => send('sensor')"
-        >
-          {{ sensor ? 'On' : 'Off' }}
-        </UiToggle>
-        <div class="size-10 *:m-auto content-center">
-          <IconsLightBulbFilled v-if="led" />
-          <IconsLightBulb v-else />
-        </div>
-      </div>
+  <div class="w-screen h-screen content-center">
+    <div class="container grid gap-8 sm:grid-cols-2 lg:grid-cols-3">
+      <UiCard>
+        <UiCardHeader>
+          <UiCardTitle class="leading-9">LED</UiCardTitle>
+        </UiCardHeader>
+        <UiCardContent class="flex gap-2">
+          <UiToggle
+            class="flex-1"
+            variant="outline"
+            :pressed="led"
+            @update:pressed="() => send('led')"
+          >
+            {{ led ? 'On' : 'Off' }}
+          </UiToggle>
+          <div class="size-10 *:m-auto content-center">
+            <IconsLightBulbFilled v-if="led" />
+            <IconsLightBulb v-else />
+          </div>
+        </UiCardContent>
+      </UiCard>
+      <UiCard>
+        <UiCardHeader>
+          <div class="flex gap-4">
+            <UiCardTitle class="flex-1 leading-9">Sensor</UiCardTitle>
+            <UiButton
+              @mousedown="() => send('sensor')"
+              @mouseup="() => send('sensor')"
+              size="sm"
+            >
+              Press
+            </UiButton>
+          </div>
+        </UiCardHeader>
+        <UiCardContent class="space-y-4">
+          <div class="flex gap-2">
+            <UiToggle
+              class="flex-1"
+              variant="outline"
+              :pressed="sensor"
+              @update:pressed="() => send('sensor')"
+            >
+              {{ sensor ? 'On' : 'Off' }}
+            </UiToggle>
+            <div class="size-10 *:m-auto content-center">
+              <IconsLightBulbFilled v-if="sensor" />
+              <IconsLightBulb v-else />
+            </div>
+          </div>
+        </UiCardContent>
+      </UiCard>
     </div>
   </div>
 </template>
